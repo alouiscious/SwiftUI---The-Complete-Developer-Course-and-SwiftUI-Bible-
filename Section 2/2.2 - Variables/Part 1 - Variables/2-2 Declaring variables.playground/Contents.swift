606a308540bd1bf939ad3bbfe8e3dcc19e649eb6
Alouiscious Media <info@reneesoul.com>
2021-03-11 20:56:43 -0800

    completed chapter 2.2

---
+++ Section 2/2.2 - Variables/Part 1 - Variables/2-2 Declaring variables.playground/Contents.swift
@@ -0,0 +1,19 @@
+//Decalration and Initialization
+import UIKit
+
+var str = "Hello Playground"
+//var mynumber: Int  = declares the a variable of the integer type
+//mynumber = 5 : decalres a inferred integer
+
+//var myNumber: Int = 5 : declares the a variable of the integer type and the value is 5
+//myNumber = 87
+//
+//var myFavorite: Float = 14.129 : declares the a variable of the float type and the value is 14.129
+
+var myNumber = 5
+
+var myOtherNumber = myNumber //allows assignment of the new variable a value equal to the firsts variables value of 5
+
+
+
+
