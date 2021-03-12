c5a27416dd120dc2f03e744b560c2d5d59908135
Alouiscious Media <info@reneesoul.com>
2021-03-11 22:31:07 -0800

    Update Contents.swift

--- Section 2/2.2 - Variables/Part 3 - Constants/2-16 Declaring and initializing a constant.playground/Contents.swift
+++ Section 2/2.2 - Variables/Part 3 - Constants/2-16 Declaring and initializing a constant.playground/Contents.swift
@@ -1,2 +1,8 @@
 let mynumber = 5
 
+/*
+ Constants help secure the way data is stored
+ and secures the value wont change.
+ */
+
+// mynumber = 4  produces an error informing you cannot assign a new value
